e29842b53179db10f5e764e747ef2cc51788fc59
ytxu <roy@newchama.com>
2016-08-25 10:10:30 +0800

    完成了findmax findmin 功能；

--- src/cn/ytxu/data_structure_and_algorithm_analysis_learning_notes/guide_01/OrderedCollection.java
+++ src/cn/ytxu/data_structure_and_algorithm_analysis_learning_notes/guide_01/OrderedCollection.java
@@ -12,12 +12,51 @@ public class OrderedCollection<E extends Comparable<E>> extends GenericCollectio
 
     @Override
     public E findMax() {
-        return null;
+        return find(new Callback<E>() {
+            @Override
+            public boolean isNotTargetElement(E element, int i) {
+                return isNotMax(element, i);
+            }
+        });
+    }
+
+    private boolean isNotMax(E element, int i) {
+        if (element == null) return true;
+        return element.compareTo(get(i)) < 0;
     }
 
     @Override
     public E findMin() {
-        return null;
+        return find(new Callback<E>() {
+            @Override
+            public boolean isNotTargetElement(E element, int i) {
+                return isNotMin(element, i);
+            }
+        });
+    }
+
+    private boolean isNotMin(E element, int i) {
+        if (element == null) return false;
+
+        return element.compareTo(get(i)) > 0;
+    }
+
+    private E find(Callback callback) {
+        if (size() <= 0) {
+            return null;
+        }
+
+        E element = get(0);
+        for (int i = 1; i < size(); i++) {
+            if (callback.isNotTargetElement(element, i)) {
+                element = get(i);
+            }
+        }
+        return element;
+    }
+
+    private interface Callback<E> {
+        boolean isNotTargetElement(E element, int i);
     }
 
 }
