c4d573926e31c98ae8cb58745f0597f2a51190bd
ytxu <roy@newchama.com>
2016-11-06 17:26:51 +0800

    add the best answer;
    =====================
    pass(personNumber, transferTimes);// array
    //        pass2(personNumber, transferTimes);
    //        josephus(personNumber, transferTimes);
            josephus2(personNumber, transferTimes);// linked
            josephus3(personNumber, transferTimes, false);// linked
            josephus3(personNumber, transferTimes, true);// array-->best
    =====================
    
    [30]
    count:3799937113, total duration:25898
    []
    count:3383169065, total duration:26474
    stop this transfer, the last element is [8304]
    []
    count:3383169065, total duration:24900
    stop this transfer, the last element is [8304]
    []
    count:3383169065, total duration:11573
    stop this transfer, the last element is [8304]
    
    [15]
    count:3244349553, total duration:18568
    []
    count:2830748540, total duration:23242
    stop this transfer, the last element is [102397]
    []
    count:2830748540, total duration:22559
    stop this transfer, the last element is [102397]
    []
    count:2830748540, total duration:9342
    stop this transfer, the last element is [102397]
    =================

--- src/cn/ytxu/data_structure_and_algorithm_analysis_learning_notes/table_stack_queue_03/Question3_06.java
+++ src/cn/ytxu/data_structure_and_algorithm_analysis_learning_notes/table_stack_queue_03/Question3_06.java
@@ -23,13 +23,18 @@ public class Question3_06 {
 //        compare(15455, 55523);
 //        compare(45155, 999);
 //        compare(45155, 55523);
-        compare(145155, 5233);
+//        compare(145155, 5233);
+//        compare(145155, 52334);
+        compare(145155, 32334);
     }
 
     private static void compare(int personNumber, int transferTimes) {
         pass(personNumber, transferTimes);
-        josephus(personNumber, transferTimes);
+//        pass2(personNumber, transferTimes);
+//        josephus(personNumber, transferTimes);
         josephus2(personNumber, transferTimes);
+        josephus3(personNumber, transferTimes, false);
+        josephus3(personNumber, transferTimes, true);
         System.out.println("=================");
     }
 
@@ -38,9 +43,58 @@ public class Question3_06 {
         PrintBufferHelper printHelper = new PrintBufferHelper();
 
         int i, j, mPrime, numLeft;
-        ArrayList<Integer> L = new ArrayList<>();
+        ArrayList<Integer> L = new ArrayList<>(n);
         for (i = 1; i <= n; i++)
             L.add(i);
+        timeHelper.duration();
+        ListIterator<Integer> iter = L.listIterator();
+        Integer item = 0;
+        numLeft = n;
+        mPrime = m % n;
+        for (i = 0; i < n; i++) {
+            timeHelper.cpp();
+            mPrime = m % numLeft;
+            if (mPrime <= numLeft / 2) {
+                timeHelper.cpp();
+                if (iter.hasNext())
+                    item = iter.next();
+                for (j = 0; j < mPrime; j++) {
+                    timeHelper.cpp();
+                    if (!iter.hasNext())
+                        iter = L.listIterator();
+                    item = iter.next();
+                }
+            } else {
+                for (j = 0; j < numLeft - mPrime; j++) {
+                    timeHelper.cpp();
+                    if (!iter.hasPrevious())
+                        iter = L.listIterator(L.size());
+                    item = iter.previous();
+                }
+            }
+            printHelper.printDelay(item + ",\t");
+            iter.remove();
+            if (!iter.hasNext())
+                iter = L.listIterator();
+//            System.out.println();
+//            for (Integer x : L)
+//                System.out.print(x + " ");
+//            System.out.println();
+            numLeft--;
+        }
+        printHelper.flush();
+        timeHelper.end().printLog();
+    }
+
+    public static void pass2(int n, int m) {
+        TimeHelper timeHelper = new TimeHelper();
+        PrintBufferHelper printHelper = new PrintBufferHelper();
+
+        int i, j, mPrime, numLeft;
+        LinkedList<Integer> L = new LinkedList<>();
+        for (i = 1; i <= n; i++)
+            L.add(i);
+        timeHelper.duration();
         ListIterator<Integer> iter = L.listIterator();
         Integer item = 0;
         numLeft = n;
@@ -88,7 +142,7 @@ public class Question3_06 {
         TimeHelper timeHelper = new TimeHelper();
         PrintBufferHelper printHelper = new PrintBufferHelper();
 
-        List<Integer> personNumbers = getPersonNumbers(personNumber);
+        List<Integer> personNumbers = getLinkedPersonNumbers(personNumber);
         ListIterator<Integer> personIter = personNumbers.listIterator();
 
         int transferCount = 0;
@@ -121,14 +175,19 @@ public class Question3_06 {
         System.out.println("stop this transfer, the last element is " + personNumbers.toString());
     }
 
-    private static List<Integer> getPersonNumbers(int personNumber) {
+    private static List<Integer> getArrayPersonNumbers(int personNumber) {
+        List<Integer> personNumbers = new ArrayList<>(personNumber);
+        for (int i = 0; i < personNumber; i++) {
+            personNumbers.add(i + 1);
+        }
+        return personNumbers;
+    }
+
+    private static List<Integer> getLinkedPersonNumbers(int personNumber) {
         List<Integer> personNumbers = new LinkedList<>();
         for (int i = 0; i < personNumber; i++) {
             personNumbers.add(i + 1);
         }
-
-        LinkedList lst;
-        lst.
         return personNumbers;
     }
 
@@ -151,7 +210,7 @@ public class Question3_06 {
         TimeHelper timeHelper = new TimeHelper();
         PrintBufferHelper printHelper = new PrintBufferHelper();
 
-        List<Integer> personNumbers = getPersonNumbers(personNumber);
+        List<Integer> personNumbers = getLinkedPersonNumbers(personNumber);
         ListIterator<Integer> personIter = personNumbers.listIterator();
 
         int currIndex = 0;// iterator index
@@ -177,6 +236,96 @@ public class Question3_06 {
                         break;
                     } else {
                         currIndex++;
+                    }
+                }
+            } else {
+                if (targetIndex < currIndex - targetIndex) {// t:3, c:100
+                    personIter = personNumbers.listIterator();
+                    currIndex = 0;
+                    while (true) {
+                        timeHelper.cpp();
+                        Integer currPersonNumber = personIter.next();
+                        if (targetIndex == currIndex) {// find
+                            printHelper.printDelay(currPersonNumber + ",\t");
+                            personIter.remove();
+                            break;
+                        } else {
+                            currIndex++;
+                        }
+                    }
+                } else {// t:90, c:100
+                    // do previous for non recycler
+                    while (true) {
+                        timeHelper.cpp();
+                        if (targetIndex == currIndex) {// find
+                            Integer currPersonNumber = personIter.next();
+                            printHelper.printDelay(currPersonNumber + ",\t");
+                            personIter.remove();
+                            break;
+                        } else {
+                            personIter.previous();
+                            currIndex--;
+                        }
+                    }
+                }
+            }
+        }
+
+        printHelper.flush();
+        timeHelper.end().printLog();
+        System.out.println("stop this transfer, the last element is " + personNumbers.toString());
+    }
+
+    /**
+     * @param personNumber  围成圆圈的人的个数
+     * @param transferTimes 传递的次数
+     */
+    public static void josephus3(final int personNumber, final int transferTimes, boolean isArray) {
+        TimeHelper timeHelper = new TimeHelper();
+        PrintBufferHelper printHelper = new PrintBufferHelper();
+
+        List<Integer> personNumbers = isArray ? getArrayPersonNumbers(personNumber) : getLinkedPersonNumbers(personNumber);
+        ListIterator<Integer> personIter = personNumbers.listIterator();
+
+        int currIndex = 0;// iterator index
+
+        while (personNumbers.size() > 1) {
+            timeHelper.cpp();
+            final int currSize = personNumbers.size();
+            final int realTimes = getRealTransferTimes(personNumbers, transferTimes);
+            final int targetIndex = (realTimes + currIndex) % currSize;
+
+            if (targetIndex == currIndex) {
+                Integer currPersonNumber = personIter.next();
+                printHelper.printDelay(currPersonNumber + ",\t");
+                personIter.remove();
+            } else if (targetIndex > currIndex) {// c:3, t:100
+                // do next for non recycler
+                if (realTimes <= targetIndex) {
+                    while (true) {
+                        timeHelper.cpp();
+                        Integer currPersonNumber = personIter.next();
+                        if (targetIndex == currIndex) {// find
+                            printHelper.printDelay(currPersonNumber + ",\t");
+                            personIter.remove();
+                            break;
+                        } else {
+                            currIndex++;
+                        }
+                    }
+                } else {
+                    personIter = personNumbers.listIterator(personNumbers.size());
+                    currIndex = personNumbers.size() - 1;
+                    while (true) {
+                        timeHelper.cpp();
+                        Integer currPersonNumber = personIter.previous();
+                        if (targetIndex == currIndex) {// find
+                            printHelper.printDelay(currPersonNumber + ",\t");
+                            personIter.remove();
+                            break;
+                        } else {
+                            currIndex--;
+                        }
                     }
                 }
             } else {
--- src/cn/ytxu/util/TimeHelper.java
+++ src/cn/ytxu/util/TimeHelper.java
@@ -1,8 +1,14 @@
 package cn.ytxu.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimeHelper {
     private long count = 0;
     private long start, end;
+
+    private List<Long> times = new ArrayList<>(10);// 每次调用的时间
+    private List<String> durations = new ArrayList<>(10);// 每次调用的间隔
 
     public TimeHelper() {
         start = System.currentTimeMillis();
@@ -13,12 +19,20 @@ public class TimeHelper {
         return this;
     }
 
+    public void duration() {
+        long curr = System.currentTimeMillis();
+        durations.add("" + (curr - start));
+        times.add(curr);
+    }
+
     public TimeHelper end() {
         end = System.currentTimeMillis();
         return this;
     }
 
     public void printLog() {
-        System.out.println("count:" + count + ", duration:" + (end - start));
+        System.out.println(durations.toString());
+        System.out.println("count:" + count + ", total duration:" + (end - start));
     }
+
 }
